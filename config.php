2e081a285f6484ce4855a2400545f4c3df37a275
skr207853 <info.sumitraut@gmail.com>
2021-07-04 09:28:41 +0530

    remove config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$databaseHost = 'localhost';
-$databaseName = 'test';
-$databaseUser = 'root';
-$databasePassword = '';
-$mysqli = mysqli_connect($databaseHost, $databaseUser, $databasePassword, $databaseName);
-
-?>
